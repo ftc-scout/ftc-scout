7f9d6c090f938e5e21d4b505634d009a470d6d63
Daniel James <danieldragonhatcher@gmail.com>
2022-05-28 20:30:18 -0700

    Remove comment

--- web/src/lib/graphql/query-load.ts
+++ web/src/lib/graphql/query-load.ts
@@ -3,23 +3,6 @@ import type { Load } from "@sveltejs/kit";
 import type { OperationContext } from "@urql/svelte";
 import type { DocumentNode } from "graphql";
 
-// export function queryLoad<Data = any, Variables = object>(
-//     propName: string,
-//     query: QueryArgs<Data, Variables>
-// ): Load {
-//     return async ({ stuff }) => {
-//         let ret = {
-//             [propName]: queryStore({
-//                 ...query,
-//                 client: stuff.client!,
-//             }),
-//         };
-//         console.log("(in load) running in", browser ? "browser" : "server");
-//         console.log("(in load) greeting", ret["greeting"])
-//         return ret;
-//     };
-// }
-
 export function queryLoad<Data = any, Variables = object>(
     name: string,
     query: string | DocumentNode | TypedDocumentNode<Data, Variables>,
