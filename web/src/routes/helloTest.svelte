4c754e8d9eb99d6916661af790815ee64c6b9c9e
joncode9001 <jaytuby@gmail.com>
2022-05-28 21:30:56 -0700

    added hello world

---
+++ web/src/routes/helloTest.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+    import {
+        HelloDocument,
+        type HelloQuery,
+    } from "$lib/graphql/generated/graphql-operations";
+    import { queryLoad } from "$lib/graphql/query-load";
+    import type { OperationStore } from "@urql/svelte";
+
+    export const load = queryLoad("salutations", HelloDocument, {});
+</script>
+
+<script lang="ts">
+    export let salutations: OperationStore<HelloQuery>;
+</script>
+{$salutations.data?.helloWorld}
