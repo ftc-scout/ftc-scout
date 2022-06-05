82af55501312d9d78f18ffd9f78f2ffd2b615d86
Daniel James <danieldragonhatcher@gmail.com>
2022-06-05 11:32:07 -0700

    Extract some of the graphql stuff from __layout into its own function

---
+++ web/src/lib/graphql/graphql-setup-load.ts
@@ -0,0 +1,34 @@
+import { customCreateClient } from "$lib/graphql/client";
+import {
+    operationStore,
+    type OperationContext,
+    type OperationStore,
+    type TypedDocumentNode,
+} from "@urql/svelte";
+import type { Load } from "@sveltejs/kit";
+import type { DocumentNode } from "graphql";
+import { get } from "svelte/store";
+
+export const graphqlSetupLoad: Load = async function ({ fetch, stuff }) {
+    const client = customCreateClient(fetch);
+
+    return {
+        stuff: {
+            ...stuff,
+            client,
+            query: async (
+                query: string | DocumentNode | TypedDocumentNode<any, any>,
+                variables?: any,
+                context?: Partial<OperationContext & { pause: boolean }>
+            ): Promise<OperationStore> => {
+                const store = operationStore(query, variables, context);
+                const result = await client
+                    .query(store.query, store.variables, store.context)
+                    .toPromise();
+                Object.assign(get(store), result);
+                return store;
+            },
+        },
+        props: { client },
+    };
+};
--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -1,43 +1,12 @@
 <script context="module" lang="ts">
-    import { customCreateClient } from "$lib/graphql/client";
-    import {
-        operationStore,
-        type OperationContext,
-        type OperationStore,
-        type TypedDocumentNode,
-    } from "@urql/svelte";
-    import type { Load } from "@sveltejs/kit";
+    import { graphqlSetupLoad } from "../lib/graphql/graphql-setup-load";
 
-    export const load: Load = async function ({ fetch, stuff }) {
-        const client = customCreateClient(fetch);
-
-        return {
-            stuff: {
-                ...stuff,
-                client,
-                query: async (
-                    query: string | DocumentNode | TypedDocumentNode<any, any>,
-                    variables?: any,
-                    context?: Partial<OperationContext & { pause: boolean }>
-                ): Promise<OperationStore> => {
-                    const store = operationStore(query, variables, context);
-                    const result = await client
-                        .query(store.query, store.variables, store.context)
-                        .toPromise();
-                    Object.assign(get(store), result);
-                    return store;
-                },
-            },
-            props: { client },
-        };
-    };
+    export let load = graphqlSetupLoad;
 </script>
 
 <script lang="ts">
     import type { Client } from "@urql/svelte";
     import { setClient } from "@urql/svelte";
-    import type { DocumentNode } from "graphql";
-    import { get } from "svelte/store";
 
     export let client: Client;
     setClient(client);
@@ -47,7 +16,13 @@
 
 <style>
     :global(:root) {
-        --theme-color: #2c666e;
         font-family: sans-serif;
+
+        --background-color: #f5f6f7;
+        --foreground-color: #ffffff;
+        --theme-color: #2c666e;
+
+        --text-color: #000000;
+        --secondary-text-color: #636466;
     }
 </style>
