72219be4f2d9b028d5343922f8c7fae915d99c61
Daniel James <danieldragonhatcher@gmail.com>
2022-06-23 10:46:20 -0700

    Some improvements to the make request function

--- server/src/ftc-api/make-request.ts
+++ server/src/ftc-api/make-request.ts
@@ -1,11 +1,26 @@
 import "isomorphic-fetch";
 import { FTC_API_KEY } from "../constants";
 
+let lastRequest = 0;
+
 export async function makeRequest(
     url: string,
     params: Record<string, any> = {},
     sinceDate: Date | null
 ): Promise<any | null> {
+    // Rate limit ourselves to 5 requests per second
+    while (true) {
+        let now = Date.now();
+
+        if (lastRequest > now - 200) {
+            await new Promise((r) => setTimeout(r, lastRequest + 200 - now));
+        } else {
+            break;
+        }
+    }
+
+    lastRequest = Date.now();
+
     const paramsString = Object.entries(params)
         .map((x) => `${x[0]}=${x[1]}`)
         .join("&");
@@ -19,9 +34,11 @@ export async function makeRequest(
             },
         }
     );
-    try {
-        return await response.json();
-    } catch {
+
+    let text = (await response.text()).trim();
+    if (text == "") {
         return null;
+    } else {
+        return JSON.parse(text);
     }
 }
