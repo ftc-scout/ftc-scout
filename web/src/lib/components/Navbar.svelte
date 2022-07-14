bd7f008efee491e8c203c6deefb2fc70dfaa08dd
Daniel James <danieldragonhatcher@gmail.com>
2022-07-14 12:42:03 -0400

    Searchbar prototype

--- web/src/lib/components/Navbar.svelte
+++ web/src/lib/components/Navbar.svelte
@@ -2,30 +2,64 @@
     import {
         LogoutDocument,
         MeDocument,
+        SearchDocument,
+        type SearchQuery,
     } from "$lib/graphql/generated/graphql-operations";
-    import { mutation } from "svelte-apollo";
+    import { mutation, query } from "svelte-apollo";
+    import { goto } from "$app/navigation";
 
     export let meData: any; // TODO type
 
     const logoutMutation = mutation(LogoutDocument);
     const logout = async () =>
         await logoutMutation({ refetchQueries: [{ query: MeDocument }] });
-    import { goto } from "$app/navigation";
-    let searchValue = "";
+    let searchText = "";
 
+    let searchResults = query(SearchDocument, { variables: { searchText } });
+    $: searchResults.refetch({ searchText });
+
+    let searchData: SearchQuery["search"] | undefined;
+    $: searchData = ($searchResults.data as any)?.search as any;
+
+    function tryGoto() {
+        if (searchData && searchData.length) {
+            goto(`/teams/${searchData[0].team.number}`);
+            searchText = "";
+        }
+    }
 </script>
 
 <nav>
-    <form autocomplete="off" on:submit|preventDefault={() => goto(`/teams/${searchValue}`)}>
+    <form autocomplete="off" on:submit|preventDefault={tryGoto}>
         <input
-        class="searchCSS"
+            class="searchCSS"
             type="search"
-            id ="searchInput"
+            id="searchInput"
             name="teamsearch"
-            bind:value={searchValue}
+            bind:value={searchText}
             placeholder="Input team..."
         />
-        <div id="result"></div>
+        {#if searchText && searchData && searchData.length}
+            <div class="result">
+                <div class="header">Teams</div>
+                <ol>
+                    {#each searchData as oneSearchRes}
+                        <li>
+                            <a
+                                sveltekit:prefetch
+                                href={`/teams/${oneSearchRes.team.number}`}
+                                on:click={() => (searchText = "")}
+                            >
+                                {@html oneSearchRes.highlighted}
+                            </a>
+                        </li>
+                    {/each}
+                    <!-- <li><a href="/teams/16321">16321 X Drive</a></li>
+                    <li><a href="/teams/16321">16321 X Drive</a></li>
+                    <li><a href="/teams/16321">16321 X Drive</a></li> -->
+                </ol>
+            </div>
+        {/if}
     </form>
     {#if meData}
         <button class="login-button" on:click={logout}>Log Out</button>
@@ -37,15 +71,62 @@
 </nav>
 
 <style>
+    form {
+        position: relative;
+    }
 
+    .result {
+        position: absolute;
+        top: 60px;
 
-.searchCSS {
-  border: 1px solid transparent;
-  background-color: #f1f1f1;
-  padding: 9px;
-  font-size: 18px;
-  border-radius: var(--pill-border-radius);
-}
+        background: var(--foreground-color);
+        width: 100%;
+
+        padding: var(--padding);
+        border-radius: 8px;
+
+        box-shadow: -2px 2px 10px 3px #e0e0e0;
+    }
+
+    .result .header {
+        margin-bottom: var(--small-gap);
+        font-weight: bold;
+        color: var(--secondary-text-color);
+    }
+
+    .result ol {
+        padding: 0;
+        margin: 0;
+    }
+
+    .result li {
+        list-style: none;
+        display: block;
+        width: 100%;
+    }
+
+    .result a {
+        display: block;
+        width: 100%;
+        height: 100%;
+
+        color: inherit;
+
+        padding: var(--padding);
+    }
+
+    .result li:hover {
+        background: var(--hover-color);
+    }
+
+    .searchCSS {
+        border: 1px solid transparent;
+        background-color: #f1f1f1;
+        padding: 9px;
+        font-size: 18px;
+        border-radius: var(--pill-border-radius);
+    }
+
     nav {
         display: flex;
         flex-direction: row;
