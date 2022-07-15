eeb16f80b64e46828328969fe20d837c5c38e1ad
Daniel James <danieldragonhatcher@gmail.com>
2022-07-15 10:01:34 -0400

    Search results caching

--- server/src/graphql/resolvers/SearchResolver.ts
+++ server/src/graphql/resolvers/SearchResolver.ts
@@ -1,6 +1,7 @@
 import FuzzySet from "fuzzyset";
 import fuzzysort from "fuzzysort";
 import { Arg, Field, ObjectType, Query, Resolver } from "type-graphql";
+import { MINUTE_MS } from "../../constants";
 import { Team } from "../../db/entities/Team";
 
 @ObjectType()
@@ -27,20 +28,46 @@ export class SearchResolver {
     ): Promise<TeamSearchResult[]> {
         if (searchText.length > 100) return [];
 
-        let teams = await Team.find(); // TODO cache this
+        await updateCache();
 
         // fuzzysort results have to contain all the text in the search (but can be be jumbled).
         // Therefore we check this first.
-        let results = resultsStrict(teams, searchText);
+        let results = resultsStrict(searchText);
 
         if (results.length == 0) {
             // If fuzzysort didn't find anything it is likely that the user made a misspelling.
             // Therefore we will try fuzzyset which is more lenient.
-            results = resultsLax(teams, searchText);
+            results = resultsLax(searchText);
         }
         return results;
     }
 }
+
+const CACHE_UPDATE_TIME = MINUTE_MS * 5;
+
+async function updateCache() {
+    let now = new Date();
+    if (
+        new Date().getTime() >
+        teamsSearchCache.lastUpdate.getTime() + CACHE_UPDATE_TIME
+    ) {
+        let teams = await Team.find();
+
+        teams.forEach((t) => (t.number = ("" + t.number) as any));
+        teamsSearchCache.teams = teams;
+
+        let combined = teams.map((t) => `${t.number} ${t.name}`.toLowerCase());
+        teamsSearchCache.fuzzyset = FuzzySet(combined);
+
+        teamsSearchCache.lastUpdate = now;
+    }
+}
+
+let teamsSearchCache = {
+    lastUpdate: new Date(0),
+    fuzzyset: FuzzySet([]),
+    teams: [] as Team[],
+};
 
 function getHighlightText(res: Fuzzysort.KeysResult<Team>): string {
     let numPart = fuzzysort.highlight(res[0]) ?? "" + res.obj.number;
@@ -48,9 +75,8 @@ function getHighlightText(res: Fuzzysort.KeysResult<Team>): string {
     return `${numPart} ${namePart}`;
 }
 
-function resultsStrict(teams: Team[], searchText: string): TeamSearchResult[] {
-    teams.forEach((t) => (t.number = ("" + t.number) as any));
-    let results = fuzzysort.go(searchText, teams, {
+function resultsStrict(searchText: string): TeamSearchResult[] {
+    let results = fuzzysort.go(searchText, teamsSearchCache.teams, {
         limit: NUM_RESULTS,
         threshold: -10000,
         keys: ["number", "name"],
@@ -58,12 +84,15 @@ function resultsStrict(teams: Team[], searchText: string): TeamSearchResult[] {
     return results.map((r) => new TeamSearchResult(r.obj, getHighlightText(r)));
 }
 
-function resultsLax(teams: Team[], searchText: string): TeamSearchResult[] {
-    let combined = teams.map((t) => `${t.number} ${t.name}`.toLowerCase());
-    let set = FuzzySet(combined);
-    let results = set.get(searchText.toLowerCase(), undefined, 0.25) ?? [];
+function resultsLax(searchText: string): TeamSearchResult[] {
+    let results =
+        teamsSearchCache.fuzzyset.get(
+            searchText.toLowerCase(),
+            undefined,
+            0.25
+        ) ?? [];
     return results.slice(0, NUM_RESULTS).map((r) => {
-        let originalTeam = teams.filter(
+        let originalTeam = teamsSearchCache.teams.filter(
             (t) => `${t.number} ${t.name}`.toLowerCase() == r[1]
         )[0];
         return new TeamSearchResult(
