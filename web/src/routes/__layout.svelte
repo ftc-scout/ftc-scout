e0be003947dc516bec78c69c850b870adecd3059
Daniel James <danieldragonhatcher@gmail.com>
2022-07-22 13:31:43 -0400

    Placeholder favicon

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -21,6 +21,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900&display=swap" rel="stylesheet" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2c666e" />
+    <meta name="msapplication-TileColor" content="#2c666e" />
+    <meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <Navbar {meData} />
