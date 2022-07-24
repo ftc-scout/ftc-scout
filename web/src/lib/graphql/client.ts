ad04062a8bfa8ea86147b53c67e9ee32952f5e49
Daniel James <danieldragonhatcher@gmail.com>
2022-07-24 17:54:59 -0400

    Fix stat merging issue

--- web/src/lib/graphql/client.ts
+++ web/src/lib/graphql/client.ts
@@ -25,7 +25,16 @@ const cache = new InMemoryCache({
         //     keyFields: ["season", "eventCode", "matchId"],
         // },
         TeamMatchParticipation: { keyFields: ["season", "eventCode", "matchId", "teamNumber"] },
-        TeamEventParticipation2021: { keyFields: ["season", "eventCode", "teamNumber"] },
+        TeamEventParticipation2021: {
+            fields: {
+                stats: {
+                    merge(existing, incoming, { mergeObjects }) {
+                        return mergeObjects(existing, incoming);
+                    },
+                },
+            },
+            keyFields: ["season", "eventCode", "teamNumber"],
+        },
         Award: { keyFields: ["season", "eventCode", "awardCode", "teamNumber"] },
         SearchResults: { keyFields: ["searchText"] },
         TeamSearchResult: { keyFields: ["searchText", "highlighted"] },
