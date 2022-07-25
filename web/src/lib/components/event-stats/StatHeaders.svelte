cf6ca11ec6310061f76cbae7febfc4830b26fe48
Daniel James <danieldragonhatcher@gmail.com>
2022-07-25 15:43:08 -0400

    Draggable headers

--- web/src/lib/components/event-stats/StatHeaders.svelte
+++ web/src/lib/components/event-stats/StatHeaders.svelte
@@ -22,18 +22,90 @@
             };
         }
     }
+
+    function draggable(element: HTMLElement) {
+        let moving = false;
+        let mousePos = { x: 0, y: 0 };
+        let placeholder: HTMLElement | null = null;
+        let parent: HTMLElement | null = null;
+
+        function moveTo() {
+            element.style.left = `${mousePos.x}px`;
+            element.style.top = `${mousePos.y}px`;
+        }
+
+        function down(e: MouseEvent) {
+            if (e.target == element) {
+                moving = true;
+
+                let pos = element.getBoundingClientRect();
+                let width = pos.right - pos.left; // using these of clientWidth/Height gives values to the exact subpixels.
+                let height = pos.bottom - pos.top;
+                parent = element.parentElement!;
+                mousePos = { x: pos.left, y: pos.top };
+
+                placeholder = document.createElement("td");
+                placeholder.style.minWidth = `${width}px`;
+                placeholder.style.height = `${height}px`;
+                placeholder.style.background = "var(--neutral-separator-color)";
+                parent.replaceChild(placeholder, element);
+
+                document.body.appendChild(element);
+                element.style.position = "absolute";
+                element.style.display = "block";
+                element.style.width = `${width}px`;
+                element.style.height = `${height}px`;
+                element.style.cursor = "grabbing";
+                moveTo();
+            }
+        }
+
+        function move(e: MouseEvent) {
+            if (moving) {
+                mousePos.x += e.movementX;
+                mousePos.y += e.movementY;
+                moveTo();
+            }
+        }
+
+        function up() {
+            if (moving) {
+                moving = false;
+                element.style.position = "";
+                element.style.left = "";
+                element.style.top = "";
+                element.style.width = "";
+                element.style.height = "";
+                element.style.display = "";
+                element.style.cursor = "";
+                parent?.replaceChild(element, placeholder!);
+            }
+        }
+
+        document.addEventListener("mousedown", down);
+        document.addEventListener("mousemove", move);
+        document.addEventListener("mouseup", up);
+
+        return {
+            destroy() {
+                document.removeEventListener("mousedown", down);
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+            },
+        };
+    }
 </script>
 
 <thead>
     {#each shownStats as shownStat}
         {@const mySort = shownStat == sort?.stat ? sort.type : SortType.NONE}
         {#if shownStat == "team"}
-            <td class="team">
+            <td class="team white" use:draggable>
                 Team
                 <SortButton sort={mySort} on:click={() => handleClick(shownStat)} />
             </td>
         {:else}
-            <td class={shownStat.color} title={shownStat.longName}>
+            <td class={shownStat.color} title={shownStat.longName} use:draggable>
                 {shownStat.shortName}
                 <SortButton sort={mySort} on:click={() => handleClick(shownStat)} />
             </td>
@@ -47,14 +119,18 @@
         font-weight: bold;
         text-align: center;
         white-space: nowrap;
+
+        user-select: none;
+
+        cursor: grab;
     }
 
     td.team {
         width: 100%;
     }
 
-    td.team,
     td.white {
+        background: var(--foreground-color);
         box-shadow: rgb(0 0 0 / 14%) 0px -4px 4px -2px inset;
     }
 
@@ -82,4 +158,30 @@
         background: var(--purple-stat-color);
         color: var(--stat-heading-text-color);
     }
+
+    td.moving {
+        content: "";
+        background: var(--neutral-separator-color);
+        box-shadow: none;
+        color: transparent;
+        cursor: grabbing;
+    }
+
+    #moving-element {
+        position: absolute;
+        left: 100px;
+        top: 100px;
+        padding: var(--small-padding);
+        background-color: red;
+        color: white;
+        /* cursor: grabbing; */
+    }
+
+    /* :global(.something-moving > *) {
+        cursor: grabbing !important;
+    } */
+
+    /* td.moving :global(*) {
+        display: none;
+    } */
 </style>
