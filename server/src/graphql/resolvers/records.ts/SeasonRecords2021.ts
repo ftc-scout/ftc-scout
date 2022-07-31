1c20c9cd12d874c27204739dae120af5f2492bb1
Daniel James <danieldragonhatcher@gmail.com>
2022-07-30 21:15:35 -0400

    Much improved records querying using query builder

--- server/src/graphql/resolvers/records.ts/SeasonRecords2021.ts
+++ server/src/graphql/resolvers/records.ts/SeasonRecords2021.ts
@@ -1,12 +1,11 @@
 import { Arg, Field, Float, InputType, Int, ObjectType, Query, registerEnumType, Resolver } from "type-graphql";
-import { FindManyOptions, FindOptionsOrder, FindOptionsWhere, Raw } from "typeorm";
+import { Brackets, SelectQueryBuilder, WhereExpressionBuilder } from "typeorm";
+import { DATA_SOURCE } from "../../../db/data-source";
 import { TeamEventParticipation2021 } from "../../../db/entities/team-event-participation/TeamEventParticipation2021";
 import { TepStats2021 } from "../../../db/entities/team-event-participation/TepStats2021";
 import { CompareOperator, compareOpToSql } from "./CompareOperator";
-import { EventTypes, getWhereClauseForEventTypes } from "./EventTypes";
+import { EventTypes } from "./EventTypes";
 import { Order } from "./Order";
-
-// I hate everything in this file
 
 @ObjectType()
 class TEP2021Records {
@@ -176,27 +175,15 @@ class TEP2021Ordering {
     @Field(() => Order)
     order!: Order;
 
-    toSingleFindOptions(): FindOptionsOrder<TeamEventParticipation2021> | undefined {
-        let direction: "asc" | "desc" = this.order == Order.ASC ? "asc" : "desc";
+    addSelfToQuery(
+        query: SelectQueryBuilder<TeamEventParticipation2021>
+    ): SelectQueryBuilder<TeamEventParticipation2021> {
+        let direction: "ASC" | "DESC" = this.order == Order.ASC ? "ASC" : "DESC";
+        let sqlName = this.field.toSqlName();
 
-        if (this.field.group == null) {
-            let fn = getFieldNameSingular(this.field.fieldName);
-            return fn ? { [fn]: direction } : undefined;
-        } else {
-            let group = getGroupName(this.field.group);
-            let fn = getFieldNameGroup(this.field.fieldName);
-            return fn ? { [group]: { [fn]: direction } } : undefined;
-        }
-    }
+        if (!sqlName) return query;
 
-    static toFindOptions(orders: TEP2021Ordering[]): FindOptionsOrder<TeamEventParticipation2021> | undefined {
-        let x = orders.reduce((a, b) => {
-            return {
-                ...a,
-                ...b.toSingleFindOptions(),
-            };
-        }, {} as FindOptionsOrder<TeamEventParticipation2021>);
-        return x;
+        return query.addOrderBy(`tep.${sqlName}`, direction);
     }
 }
 
@@ -207,6 +194,18 @@ class TEP2021Value {
 
     @Field(() => TEP2021Field, { nullable: true })
     field!: TEP2021Field | null;
+
+    isInvalid(): boolean {
+        return this.value == null && this.field == null;
+    }
+
+    toSql(prefix: string): string {
+        if (this.value) {
+            return "" + this.value;
+        } else {
+            return `${prefix}${this.field!.toSqlName()}`;
+        }
+    }
 }
 
 @InputType()
@@ -220,40 +219,12 @@ class TEP2021Condition {
     @Field(() => TEP2021Value)
     rhs!: TEP2021Value;
 
-    toWhereClause(): FindOptionsWhere<TeamEventParticipation2021> {
-        if (this.lhs.value == null && this.lhs.field == null && this.rhs.value == null && this.rhs.field == null)
-            return {};
+    addSelfToQuery(query: WhereExpressionBuilder): WhereExpressionBuilder {
+        if (this.lhs.isInvalid() || this.rhs.isInvalid()) return query;
 
-        let lhs: TEP2021Field;
-        let rhs: string;
-
-        let opSql = compareOpToSql(this.compareOperator);
-
-        if (typeof this.rhs.value == "number" && typeof this.lhs.value == "number") {
-            return {
-                rp: Raw((_) => `${this.lhs.value} ${opSql} ${this.rhs.value}`),
-            };
-        } else if (typeof this.lhs.value == "number") {
-            lhs = this.rhs.field as TEP2021Field;
-            rhs = "" + this.lhs.value;
-        } else if (typeof this.rhs.value == "number") {
-            lhs = this.lhs.field as TEP2021Field;
-            rhs = "" + this.rhs.value;
-        } else {
-            lhs = this.lhs.field as TEP2021Field;
-            let temp = this.rhs.field!.toSqlName();
-            if (temp == null) return {};
-            rhs = temp;
-        }
-
-        if (lhs.group == null) {
-            let fn = getFieldNameSingular(lhs.fieldName);
-            return fn ? { [fn]: Raw((a) => `${a} ${opSql} ${rhs}`) } : {};
-        } else {
-            let group = getGroupName(lhs.group);
-            let fn = getFieldNameGroup(lhs.fieldName);
-            return fn ? { [group]: { [fn]: Raw((a) => `${a} ${opSql} ${rhs}`) } } : {};
-        }
+        return query.andWhere(
+            `${this.lhs.toSql("tep.")} ${compareOpToSql(this.compareOperator)} ${this.rhs.toSql("tep.")}`
+        );
     }
 }
 
@@ -262,19 +233,14 @@ class TEP2021AndGroup {
     @Field(() => [TEP2021Condition])
     conditions!: TEP2021Condition[];
 
-    toWhereClause(eventTypes: EventTypes): FindOptionsWhere<TeamEventParticipation2021> {
-        let ret = this.conditions.slice(0, 10).reduce(
-            (a, b) => ({
-                ...a,
-                ...b.toWhereClause(),
-            }),
-            { hasStats: true } as FindOptionsWhere<TeamEventParticipation2021>
-        );
-
-        let event = getWhereClauseForEventTypes(eventTypes);
-        if (event) ret.event = event;
-
-        return ret;
+    toQueryBrackets(): Brackets {
+        return new Brackets((qb) => {
+            let query = qb.where("TRUE");
+            for (let condition of this.conditions.slice(0, 10)) {
+                query = condition.addSelfToQuery(query);
+            }
+            return query;
+        });
     }
 }
 
@@ -283,8 +249,18 @@ class TEP2021OrGroup {
     @Field(() => [TEP2021AndGroup])
     andGroups!: TEP2021AndGroup[];
 
-    toWhereClause(eventTypes: EventTypes): FindOptionsWhere<TeamEventParticipation2021>[] {
-        return this.andGroups.slice(0, 10).map((g) => g.toWhereClause(eventTypes));
+    addSelfToQuery(
+        query: SelectQueryBuilder<TeamEventParticipation2021>
+    ): SelectQueryBuilder<TeamEventParticipation2021> {
+        return query.andWhere(
+            new Brackets((qb) => {
+                let query = qb.where("FALSE");
+                for (let and of this.andGroups.slice(0, 10)) {
+                    query = query.orWhere(and.toQueryBrackets());
+                }
+                return query;
+            })
+        );
     }
 }
 
@@ -298,36 +274,33 @@ export class SeasonRecords2021Resolver {
         @Arg("take", () => Int) takeIn: number,
         @Arg("skip", () => Int) skip: number
     ): Promise<TEP2021Records> {
-        try {
-            let take = Math.min(takeIn, 50);
+        let limit = Math.min(takeIn, 50);
 
-            let options: FindManyOptions<TeamEventParticipation2021> = {
-                relations: {
-                    event: true,
-                },
-                where: filters.toWhereClause(eventTypes),
-                take,
-                skip,
-            };
+        let query = DATA_SOURCE.getRepository(TeamEventParticipation2021)
+            .createQueryBuilder("tep")
+            .leftJoinAndSelect("tep.event", "e")
+            .where("tep.hasStats")
+            .limit(limit)
+            .offset(skip);
 
-            let order = TEP2021Ordering.toFindOptions(orderIn.slice(0, 5));
-            if (order) {
-                options.order = order;
-            }
-
-            let [teps, count] = await TeamEventParticipation2021.findAndCount(options);
-
-            return {
-                teps,
-                offset: skip,
-                count,
-            };
-        } catch (e) {
-            return {
-                teps: [],
-                offset: skip,
-                count: 0,
-            };
+        if (eventTypes == EventTypes.REMOTE) {
+            query = query.andWhere("e.remote");
+        } else if (eventTypes == EventTypes.TRAD) {
+            query = query.andWhere("NOT e.remote");
         }
+
+        for (let order of orderIn.slice(0, 5)) {
+            query = order.addSelfToQuery(query);
+        }
+
+        query = filters.addSelfToQuery(query as any);
+
+        let [teps, count] = await query.getManyAndCount();
+
+        return {
+            teps,
+            count,
+            offset: skip,
+        };
     }
 }
