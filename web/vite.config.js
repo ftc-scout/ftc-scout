d7a602fad634f6181d996cce1696768513b3e8b5
Daniel James <danieldragonhatcher@gmail.com>
2022-08-19 17:09:52 -0700

    Make preview work

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,14 +1,17 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 
 /** @type {import('vite').UserConfig} */
-const config = {
+const config = ({ mode }) => ({
     ssr: {
         noExternal: ["@fortawesome/free-solid-svg-icons", "@apollo/client", "svelte-apollo"],
     },
     optimizeDeps: {
         exclude: ["@apollo/client", "svelte-apollo"],
     },
+    define: {
+        __DEV__: (mode === "development").toString(),
+    },
     plugins: [sveltekit()],
-};
+});
 
 export default config;
