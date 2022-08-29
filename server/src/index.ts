f0a25a994a2724f97b4d65d3f515eb7dc08f744d
Daniel James <danieldragonhatcher@gmail.com>
2022-08-28 19:18:12 -0700

    Change working directory

--- server/src/index.ts
+++ server/src/index.ts
@@ -66,7 +66,7 @@ async function main() {
     // Start the server
     app.listen(SERVER_PORT, () => {
         console.log(`Server started and listening on localhost:${SERVER_PORT}.`);
-        console.log("Test 3");
+        console.log("Test 4");
     });
 
     setupApiWatchers();
