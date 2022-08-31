6d5a42fe5c93991f25dbf3b8d125b40ccca47539
Daniel James <danieldragonhatcher@gmail.com>
2022-08-30 17:42:41 -0700

    Attempt to fix safari dropdown style issues

--- web/src/lib/components/Dropdown.svelte
+++ web/src/lib/components/Dropdown.svelte
@@ -5,13 +5,21 @@
     export let style = "";
 </script>
 
-<select {style} bind:value>
-    {#each items as item}
-        <option>{item}</option>
-    {/each}
-</select>
+<div {style}>
+    <select bind:value>
+        {#each items as item}
+            <option>{item}</option>
+        {/each}
+    </select>
+</div>
 
 <style>
+    div {
+        background: var(--foreground-color);
+        box-shadow: var(--shadow-color) 0px 2px 5px -1px, var(--shadow-color) 0px 1px 3px -1px;
+        border-radius: 8px;
+    }
+
     select {
         font: var(--main-font);
 
@@ -21,8 +29,10 @@
         cursor: pointer;
 
         padding: var(--small-padding);
-        box-shadow: var(--shadow-color) 0px 2px 5px -1px, var(--shadow-color) 0px 1px 3px -1px;
         border-radius: 8px;
+
+        height: 100%;
+        width: 100%;
     }
 
     select:hover {
