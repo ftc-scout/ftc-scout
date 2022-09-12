bf20a0571604ecbb44f1d19448bf5bae9d4881fa
Daniel James <danieldragonhatcher@gmail.com>
2022-09-11 22:09:48 -0700

    Newline in description

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -23,8 +23,7 @@
     <title>FTC Scout</title>
     <meta
         name="description"
-        content="FTCScout is a new way to track and scout FIRST Tech Challenge providing advanced statistics 
-        and data on all aspects of FTC."
+        content="FTCScout is a new way to track and scout FIRST Tech Challenge providing advanced statistics and data on all aspects of FTC."
     />
 </svelte:head>
 
