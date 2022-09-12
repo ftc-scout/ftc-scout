df6a46ec057230f34f5050648523875526ed48ee
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2022-09-12 10:50:19 -0700

    Actually change example texrt

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -35,10 +35,9 @@
         property="og:description"
         content="FTCScout is a new way to track and scout FIRST Tech Challenge providing advanced statistics and data on all aspects of FTC."
     />
-    <meta property="og:image" content="/logo/cookieandrock-logo.png" />
+    <meta property="og:image" content="/banner.png" />
     <meta name="twitter:card" content="/banner.png" />
     <meta name="theme-color" content="#2c666e" />
-    <meta property="og:image" content="/banner.png" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="620" />
     <meta property="og:image:type" content="image/png" />
