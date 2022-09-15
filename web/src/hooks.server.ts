c4babff2a3b85c1a6b5b00a0ba1ddfe078332ff6
Daniel James <danieldragonhatcher@gmail.com>
2022-09-14 17:00:31 -0700

    Fix headers

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -6,7 +6,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     response.headers.append("X-XSS-Protection", "1; mode=block");
     response.headers.append(
         "Content-Security-Policy",
-        "Content-Security-Policy: default-src 'self'; script-src 'self' googletagmanager.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com fonts.googleapis.com; frame-ancestors 'self'; upgrade-insecure-requests"
+        "default-src 'self'; script-src 'self' googletagmanager.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com fonts.googleapis.com; frame-ancestors 'self'; upgrade-insecure-requests"
     );
     return response;
 };
