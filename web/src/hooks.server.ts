0ad7c97145c66ff29e72e994e5c2796dcbccfea9
Daniel James <danieldragonhatcher@gmail.com>
2022-09-14 17:21:37 -0700

    fix headers

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -6,7 +6,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     response.headers.append("X-XSS-Protection", "1; mode=block");
     response.headers.append(
         "Content-Security-Policy",
-        "default-src 'self'; script-src 'self' 'unsafe-inline' googletagmanager.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com fonts.googleapis.com; frame-ancestors 'self'; upgrade-insecure-requests"
+        "default-src 'self' ftc-scout-server.fly.dev; script-src 'self' 'unsafe-inline' www.googletagmanager.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com fonts.googleapis.com; frame-ancestors 'self'; upgrade-insecure-requests"
     );
     return response;
 };
