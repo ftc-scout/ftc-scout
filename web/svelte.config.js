2d6505590d88edb1ae98de9d6090d3549ce8e8c3
Daniel James <danieldragonhatcher@gmail.com>
2022-09-14 20:30:55 -0700

    Use sveltekit csp

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -9,6 +9,17 @@ const config = {
         files: {
             hooks: "src/hooks.server.ts",
         },
+        csp: {
+            directives: {
+                "default-src": ["self", "ftc-scout-server.fly.dev", "www.google-analytics.com"],
+                "script-src": ["self", "www.googletagmanager.com"],
+                "style-src": ["self", "unsafe-inline", "fonts.googleapis.com"],
+                "img-src": ["self", "fonts.googleapis.com"],
+                "font-src": ["self", "fonts.gstatic.com", "fonts.googleapis.com"],
+                "frame-ancestors": ["self"],
+                "upgrade-insecure-requests": [],
+            },
+        },
     },
 };
 
