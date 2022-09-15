62a2b3d4b00c797cb9436131fa56117e1558f978
Daniel James <danieldragonhatcher@gmail.com>
2022-09-14 20:59:02 -0700

    More headers for Levi

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -5,8 +5,10 @@ export const handle: Handle = async ({ event, resolve }) => {
     const response = await resolve(event);
 
     if (IS_PROD) {
-        response.headers.append("X-Frame-Options", "SAMEORIGIN");
-        response.headers.append("X-XSS-Protection", "1; mode=block");
+        response.headers.set("X-Frame-Options", "SAMEORIGIN");
+        response.headers.set("X-XSS-Protection", "1; mode=block");
+        response.headers.set("Referrer-Policy", "same-origin");
+        response.headers.set("Permissions-Policy", "camera=(), microphone=()");
         let canonical = event.url.toString().replace(/^https:\/\/www./, "https://");
         response.headers.set("Link", `<${canonical}>; rel="canonical"`);
     }
