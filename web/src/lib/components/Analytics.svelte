6c525a931cf6986b3a4823753121ae726dcc5130
Daniel James <danieldragonhatcher@gmail.com>
2022-09-14 20:44:38 -0700

    Fix ga link

--- web/src/lib/components/Analytics.svelte
+++ web/src/lib/components/Analytics.svelte
@@ -14,6 +14,6 @@
 <svelte:head>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-5RC8F6X8NC">
     </script>
-    <script src="/static/js/ga.js">
+    <script src="/js/ga.js">
     </script>
 </svelte:head>
