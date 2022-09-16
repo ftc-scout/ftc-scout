1f100b025fc35755d16473e6b12d4b4e996348a2
Daniel James <danieldragonhatcher@gmail.com>
2022-09-15 23:05:51 -0700

    Allow introspection

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ import { DATA_SOURCE } from "./db/data-source";
 import { GraphQLContext } from "./graphql/Context";
 import { setupApiWatchers } from "./ftc-api/setup-watchers";
 import { ApolloServerLoaderPlugin } from "type-graphql-dataloader";
-import { getConnection } from "typeorm";
+import { getConnection, TreeLevelColumn } from "typeorm";
 import compression from "compression";
 import { teamBanner } from "./banners";
 import { resolve } from "path";
@@ -40,6 +40,7 @@ async function main() {
 
     // Initialize the apollo graphql server.
     const apolloServer = new ApolloServer({
+        introspection: true,
         schema: await buildSchema({
             resolvers,
             validate: false,
