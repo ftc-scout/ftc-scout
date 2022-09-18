bdc8ad018edc4117a1b3dd19be3839d393fb6099
Daniel James <danieldragonhatcher@gmail.com>
2022-09-18 16:37:57 -0700

    disable csr

--- web/src/routes/+page.ts
+++ web/src/routes/+page.ts
@@ -3,6 +3,8 @@ import { HomePageDocument } from "../lib/graphql/generated/graphql-operations";
 import { getData } from "../lib/graphql/query-load";
 import type { PageLoad } from "./$types";
 
+export const csr = false;
+
 export const load: PageLoad = async () => {
     let today = new Date();
     // Round to the hour to avoid pointless refetching.
