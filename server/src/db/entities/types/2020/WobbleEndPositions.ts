f338b5eeec8b685ec963218aa37dd3ab6520bcb1
Daniel James <danieldragonhatcher@gmail.com>
2022-10-14 09:25:41 -0700

    Fix incorrect endgame wobble scoring

--- server/src/db/entities/types/2020/WobbleEndPositions.ts
+++ server/src/db/entities/types/2020/WobbleEndPositions.ts
@@ -2,16 +2,8 @@ import { registerEnumType } from "type-graphql";
 
 export enum WobbleEndPositions {
     NONE = 0,
-    DROP_ZONE = 1,
-    START_LINE = 2,
-}
-
-export function wobbleEndPositionsFromApi(eg: "NONE" | "DROP_ZONE" | "START_LINE"): WobbleEndPositions {
-    return {
-        NONE: WobbleEndPositions.NONE,
-        DROP_ZONE: WobbleEndPositions.DROP_ZONE,
-        START_LINE: WobbleEndPositions.START_LINE,
-    }[eg];
+    START_LINE = 1,
+    DROP_ZONE = 2,
 }
 
 registerEnumType(WobbleEndPositions, {
