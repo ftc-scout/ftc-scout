f1a95028fab4e6c37c6d9c760d4300f6d0350118
Daniel James <danieldragonhatcher@gmail.com>
2023-06-08 21:19:33 -0700

    Scaffold

---
+++ packages/server/src/index.ts
@@ -0,0 +1,4 @@
+import { add } from "@ftc-scout/common";
+
+console.log("Hello from server!");
+console.log(`1 + 2 = ${add(1, 2)}`);
---
+++ packages/web/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { add } from "@ftc-scout/common";
+
+    let a: number = 1;
+    let b: number = 2;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input type="number" bind:value={a} />
+<input type="number" bind:value={b} />
+<p>{a} + {b} = {add(a, b)}</p>
