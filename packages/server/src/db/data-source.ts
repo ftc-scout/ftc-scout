5ad9153ab1451182f927a5b07b531f1e9c03b5a0
Daniel James <danieldragonhatcher@gmail.com>
2023-06-12 16:52:05 -0700

    Snake case

--- packages/server/src/db/data-source.ts
+++ packages/server/src/db/data-source.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import { DATABASE_URL, LOGGING, SYNC } from "../constants";
 import { ENTITIES } from "./entities";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
 export const DATA_SOURCE = new DataSource({
     type: "postgres",
@@ -8,4 +9,5 @@ export const DATA_SOURCE = new DataSource({
     synchronize: SYNC,
     logging: LOGGING,
     entities: ENTITIES,
+    namingStrategy: new SnakeNamingStrategy(),
 });
