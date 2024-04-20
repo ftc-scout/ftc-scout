1397a067d062d8ce1e7cb23891e3ba1a8899fcfa
Daniel James <danieldragonhatcher@gmail.com>
2024-04-20 01:36:20 -0700

    Remove logging

--- packages/web/src/lib/graphql/client.ts
+++ packages/web/src/lib/graphql/client.ts
@@ -28,19 +28,7 @@ export function getClient(
         uri: `http${s}://${env.PUBLIC_SERVER_ORIGIN}/graphql`,
         credentials: "omit",
         headers: { [env.PUBLIC_FRONTEND_CODE!]: "." },
-        fetch: (input, init) => {
-            try {
-                if (!browser) console.log(input);
-                if (!browser) console.log(">>> try");
-                let out = fetch(input, init);
-                if (!browser) console.log(">>> succeed");
-                return out;
-            } catch (e) {
-                if (!browser) console.log(">>> ERROR !!!!!!!!!");
-                if (!browser) console.log(`from: ${input}`);
-                throw e;
-            }
-        },
+        fetch,
     });
 
     let link = browser
