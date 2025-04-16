f34bcc55ff34f1991e60d5dde84a14dfc7605094
9021007 <24487638+9021007@users.noreply.github.com>
2025-04-16 15:26:34 -0700

    Remove timeout

--- packages/server/src/db/data-source.ts
+++ packages/server/src/db/data-source.ts
@@ -1,5 +1,5 @@
 import { DataSource } from "typeorm";
-import { DATABASE_URL, LOGGING, SYNC_DB, DB_TIMEOUT } from "../constants";
+import { DATABASE_URL, LOGGING, SYNC_DB } from "../constants";
 import { ENTITIES } from "./entities";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
@@ -10,9 +10,9 @@ export const DATA_SOURCE = new DataSource({
     logging: LOGGING,
     entities: ENTITIES,
     namingStrategy: new SnakeNamingStrategy(),
-    extra: {
-        connectionTimeoutMillis: DB_TIMEOUT,
-        query_timeout: DB_TIMEOUT,
-        statement_timeout: DB_TIMEOUT,
-    },
+    // extra: {
+    //     connectionTimeoutMillis: DB_TIMEOUT,
+    //     query_timeout: DB_TIMEOUT,
+    //     statement_timeout: DB_TIMEOUT,
+    // },
 });
