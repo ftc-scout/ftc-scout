2059f795a78cc7b091189dbb493444b90d91f236
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2025-10-30 16:24:54 -0700

    Show RP from decode matches

--- packages/web/src/lib/components/matches/MatchScore.svelte
+++ packages/web/src/lib/components/matches/MatchScore.svelte
@@ -24,8 +24,12 @@
 </script>
 
 <script lang="ts">
-    import { Alliance } from "@ftc-scout/common";
-    import type { FullMatchFragment } from "../../graphql/generated/graphql-operations";
+    import { Alliance, Season } from "@ftc-scout/common";
+    import {
+        TournamentLevel,
+        type FullMatchFragment,
+        type FullMatchScore2025AllianceFragment,
+    } from "../../graphql/generated/graphql-operations";
     import { prettyPrintTimeString } from "../../printers/time";
     import { createTippy } from "svelte-tippy";
     import "tippy.js/dist/tippy.css";
@@ -39,6 +43,20 @@
     export let timeZone: string;
 
     $: winner = computeWinner(match.scores);
+
+    function calculateRP(s: FullMatchFragment["scores"]) {
+        if (s?.season == Season.Decode) {
+            let red = (s as any).red as FullMatchScore2025AllianceFragment;
+            let blue = (s as any).blue as FullMatchScore2025AllianceFragment;
+            return [
+                +red.goalRp + +red.patternRp + +red.movementRp,
+                +blue.goalRp + +blue.patternRp + +blue.movementRp,
+            ];
+        } else {
+            return [0, 0];
+        }
+    }
+    $: rps = calculateRP(match.scores);
 
     const tippy = createTippy({ placement: "left", delay: [750, 0], touch: false });
     $: tip = matchTimeTip(match, timeZone, $tippyTheme);
@@ -64,13 +82,30 @@
         {#if match.scores == undefined}
             {prettyPrintTimeString(match.scheduledStartTime, timeZone)}
         {:else if "red" in match.scores}
-            <span class="left" class:winner={winner == Alliance.Red} class:tie={winner == "Tie"}>
+            <div class="left" class:winner={winner == Alliance.Red} class:tie={winner == "Tie"}>
+                <!-- // Help: Season Specific -->
+                {#if match.season == Season.Decode && match.tournamentLevel == TournamentLevel.Quals}
+                    <div class="dots red">
+                        {#each new Array(rps[0] + 3 * +(winner == Alliance.Red) + +(winner == "Tie")) as _, i}
+                            <div class="dot" style="right: calc({i} * var(--dot-stride))" />
+                        {/each}
+                    </div>
+                {/if}
+
                 {match.scores.red.totalPoints}
-            </span>
-            <span>-</span>
-            <span class="right" class:winner={winner == Alliance.Blue} class:tie={winner == "Tie"}>
+            </div>
+            <div class="minus">-</div>
+            <div class="right" class:winner={winner == Alliance.Blue} class:tie={winner == "Tie"}>
                 {match.scores.blue.totalPoints}
-            </span>
+
+                {#if match.season == Season.Decode && match.tournamentLevel == TournamentLevel.Quals}
+                    <div class="dots blue">
+                        {#each new Array(rps[1] + 3 * +(winner == Alliance.Blue) + +(winner == "Tie")) as _, i}
+                            <div class="dot" style="left: calc({i} * var(--dot-stride))" />
+                        {/each}
+                    </div>
+                {/if}
+            </div>
         {:else}
             <b>{match.scores.totalPoints}</b>
         {/if}
@@ -100,6 +135,55 @@
         padding-left: var(--md-gap);
         font-weight: bold;
         color: var(--grayed-out-text-color);
+    }
+
+    .minus {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .left,
+    .right {
+        position: relative;
+    }
+
+    .dots {
+        position: absolute;
+        bottom: 0px;
+        --dot-stride: 8px;
+        --dot-size: 6px;
+    }
+
+    @media (max-width: 1000px) {
+        .dots {
+            bottom: 1px;
+            --dot-stride: 6px;
+            --dot-size: 4px;
+        }
+    }
+
+    .left .dots {
+        right: 0px;
+    }
+
+    .right .dots {
+        left: 0px;
+    }
+
+    .dot {
+        position: absolute;
+        width: var(--dot-size);
+        height: var(--dot-size);
+        border-radius: 100%;
+        background: blue;
+    }
+
+    .red .dot {
+        background: var(--red-team-text-color);
+    }
+    .blue .dot {
+        background: var(--blue-team-text-color);
     }
 
     .red {
