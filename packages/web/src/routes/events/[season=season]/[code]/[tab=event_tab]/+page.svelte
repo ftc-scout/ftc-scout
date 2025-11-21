1222ea33fba3f984d46dc8a63cca5283822f6e66
saajbhatia <harsaajbhatia@gmail.com>
2025-11-21 12:50:36 -0800

    Add more default tabs for preview and hide advancement tab if no data
    diff --git c/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/+page.svelte i/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/+page.svelte
    index c0c6055..6031c7d 100644
    --- c/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/+page.svelte
    +++ i/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/+page.svelte
    @@ -211,8 +211,13 @@
                     [faTrophy, "Rankings", "rankings", !!rankingTeamsWithStats.length],
                     [faRankingStar, "League Rankings", "league-rankings", showLeagueRankingsTab],
                     [faLightbulb, "Insights", "insights", !!insights.length],
    -                [faMedal, "Awards", "awards", !!event.awards.length],
    -                [faChartLine, "Advancement", "advancement", !!advancementRows.length],
    +                [faMedal, "Awards", "awards", (event?.awards?.length ?? 0) > 0],
    +                [
    +                    faChartLine,
    +                    "Advancement",
    +                    "advancement",
    +                    !!advancementRows.length && !!rankingTeamsWithStats.length,
    +                ],
                     [faHashtag, "Teams", "teams", showTeamsTab],
                 ]}
                 bind:selectedTab
    diff --git c/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/Preview.svelte i/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/Preview.svelte
    index 501e25a..387ee1a 100644
    --- c/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/Preview.svelte
    +++ i/packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/Preview.svelte
    @@ -10,10 +10,9 @@
         } from "@ftc-scout/common";
         import type { EventPageQuery } from "$lib/graphql/generated/graphql-operations";
    
    -    type PreviewTeam =
    -        NonNullable<EventPageQuery["eventByCode"]>["teams"][number] & {
    -            quickOpr: number | null;
    -        };
    +    type PreviewTeam = NonNullable<EventPageQuery["eventByCode"]>["teams"][number] & {
    +        quickOpr: number | null;
    +    };
    
         export let teams: PreviewTeam[];
         export let focusedTeam: number | null;
    @@ -54,7 +53,13 @@
         $: descriptor = DESCRIPTORS[season];
         $: totalPointsKey = descriptor.pensSubtract || remote ? "totalPoints" : "totalPointsNp";
         $: defaultStatId = `${totalPointsKey}Opr`;
    -    $: defaultStats = ["team", defaultStatId];
    +    $: defaultStats = [
    +        "team",
    +        defaultStatId,
    +        "autoPointsOpr",
    +        "dcPointsOpr",
    +        ...(descriptor.hasEndgame ? ["endgamePointsOpr"] : []),
    +    ];
         $: defaultSort = { id: defaultStatId, dir: SortDir.Desc };
    
         $: safeEventName = eventName ?? "Event";

--- packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/+page.svelte
+++ packages/web/src/routes/events/[season=season]/[code]/[tab=event_tab]/+page.svelte
@@ -201,7 +201,7 @@
                 [faBolt, "Matches", "matches", (event?.matches?.length ?? 0) > 0],
                 [faTrophy, "Rankings", "rankings", !!stats.length],
                 [faLightbulb, "Insights", "insights", !!insights.length],
-                [faMedal, "Awards", "awards", !!event.awards.length],
+                [faMedal, "Awards", "awards", (event?.awards?.length ?? 0) > 0],
                 [faHashtag, `Teams (${event.teams.length})`, "teams", !!event.teams.length],
             ]}
             bind:selectedTab
