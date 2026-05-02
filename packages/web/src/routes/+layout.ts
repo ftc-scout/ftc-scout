4da385aebdd5676e24584fad2e1e2270f79238f4
9021007 <24487638+9021007@users.noreply.github.com>
2026-05-02 06:39:14 -0700

    Experiment - Disable SSR

--- packages/web/src/routes/+layout.ts
+++ packages/web/src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import type { LayoutLoad } from "./$types";
 import { getClient } from "$lib/graphql/client";
 
+export const ssr = false;
+
 export const load: LayoutLoad = async ({ fetch }) => {
     getClient(fetch);
 };
